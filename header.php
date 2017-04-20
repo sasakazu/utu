de265bfbcd7130addc1b109667f4c4c848da4358
sasakazu <dropout.college2014@gmail.com>
2017-04-20 19:06:43 +0900

    boot header done.....

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title><?php wp_title( '|', true, 'right' ); bloginfo('name'); ?></title>
+    <meta name=”viewport” content=”width=device-width, initial-scale=1.0″>
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" media="screen">
     <?php if ( is_singular() ) wp_enqueue_script( "comment-reply" ); ?>
@@ -11,7 +12,62 @@
   </head>
   <body>
 
-    <h1 id="logo">
-<a href="<?php echo home_url('/'); ?>"><span><?php bloginfo('name'); ?></span></a>
-</h1>
-<p id="description"><?php bloginfo('description'); ?></p>
+
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?><small><?php bloginfo('description'); ?></small></a>
+        </div>
+
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <!-- <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul> -->
+          <!-- <form class="navbar-form navbar-left">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form> -->
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">うつについて知る</a></li>
+            <li><a href="#">うつだった偉人</a></li>
+            <li><a href="#">うつニュース</a></li>
+            <!-- <li><a href="#">Link</a></li> -->
+            <!-- <li><a href="#">Link</a></li> -->
+            <!-- <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </li> -->
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
--- index.php
+++ index.php
@@ -1,5 +1,45 @@
 <?php get_header(); ?>
 
-<h1>hello</h1>
+
+
+
+<div class="row">
+  <div class="container">
+
+    <div class="col-md-6">
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+      <h1>NEW</h1>
+    </div>
+
+    <div class="col-md-6">
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+      <h1>RANKING</h1>
+    </div>
+
+
+  </div>
+</div>
+
+
 
 <?php get_footer(); ?>
