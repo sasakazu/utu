ca486092a7536717fadce6cbe0c908ed8304eea0
sasakazu <dropout.college2014@gmail.com>
2017-04-22 18:33:53 +0900

    index layout done.....

--- index.php
+++ index.php
@@ -4,36 +4,86 @@
 
 
 <div class="row">
-  <div class="container">
+  <div class="container top-wrapper">
 
-    <div class="col-md-6">
-      <h1>NEW</h1>
-      <h1>NEW</h1>
-      <h1>NEW</h1>
-      <h1>NEW</h1>
-      <h1>NEW</h1>
-      <h1>NEW</h1>
-      <h1>NEW</h1>
-      <h1>NEW</h1>
-      <h1>NEW</h1>
-      <h1>NEW</h1>
-      <h1>NEW</h1>
-      <h1>NEW</h1>
+    <div class="col-md-6 top-new-post">
+      <h1><i class="fa fa-newspaper-o"></i>NEW</h1>
+
+      <div class="top-new-contents row">
+
+        <div class="col-md-5">
+          <img src="https://placehold.jp/160x110.png" alt="" >
+        </div>
+
+        <div class="col-md-7">
+          <a>titletitletitletitletitletitletitletitletitletitletitle</a>
+          <p>except</p>
+        </div>
+
+      </div>
+
+      <div class="top-new-contents row">
+
+        <div class="col-md-5">
+          <img src="https://placehold.jp/160x110.png" alt="" >
+        </div>
+
+        <div class="col-md-7">
+          <a>titletitletitletitletitletitletitletitletitletitletitle</a>
+          <p>except</p>
+        </div>
+
+      </div>
+
+
+      <div class="top-new-contents row">
+
+        <div class="col-md-5">
+          <img src="https://placehold.jp/160x110.png" alt="" >
+        </div>
+
+        <div class="col-md-7">
+          <a>titletitletitletitletitletitletitletitletitletitletitle</a>
+          <p>exceptexceptexceptexcept</p>
+        </div>
+
+      </div>
+
+
+      <div class="top-new-contents row">
+
+        <div class="col-md-5">
+          <img src="https://placehold.jp/160x110.png" alt="" >
+        </div>
+
+        <div class="col-md-7">
+          <a>titletitletitletitletitletitletitletitletitletitletitle</a>
+          <p>except</p>
+        </div>
+
+      </div>
+
     </div>
 
-    <div class="col-md-6">
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
-      <h1>RANKING</h1>
+    <div class="col-md-6 top-rank-post">
+      <h1><i class="fa fa-line-chart" aria-hidden="true"></i>RANKING</h1>
+
+
+      <div class="top-rank-contents row">
+
+        <div class="col-md-5">
+          <img src="https://placehold.jp/160x110.png" alt="" >
+        </div>
+
+        <div class="col-md-7">
+          <a>titletitletitletitletitletitletitletitletitletitletitle</a>
+          <p>except</p>
+        </div>
+
+      </div>
+
+
+
     </div>
 
 
